80ed06ab3647ba0b34d345227cfb1449fdcd0093
David Hunsdon <DaveH1981@users.noreply.github.com>
2026-07-13 11:04:05 -0700

    Add files via upload

---
+++ src/DeclaringVariables.cs
@@ -0,0 +1,20 @@
+public class Program {
+    public static void Main(string[] args) {
+        //Declaring variables
+        int count = 10;
+        double total = 150.84;
+        float winPercentage = .6829268;
+        char grade = 'A';
+        bool isActive = false;
+        string userName = "Alex456";
+
+        //Print the values
+        System.Console.WriteLine("Count: " + count);
+        System.Console.WriteLine("Total: " + total);
+        System.Console.WriteLine("Win Percentage: " + winPercentage);
+        System.Console.WriteLine("Grade: " + grade);
+        System.Console.WriteLine("Active: " + isActive);
+        System.Console.WriteLine("User Name: " + userName);
+        
+    }
+}
---
+++ src/IfElseAge.cs
@@ -0,0 +1,18 @@
+int age = 13;
+
+if (age >= 2 && age < 18)
+{
+    Console.WriteLine("You're a kid!");
+}
+else if (age < 2)
+{
+    Console.WriteLine("You're a baby! Goo-goo gaga!");
+}
+else if (age > 120)
+{
+    Console.WriteLine("Whoa There! Nobody is THAT old!");
+}
+else
+{
+    Console.WriteLine("You're an adult!");
+}
